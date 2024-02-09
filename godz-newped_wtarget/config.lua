fd7366829eaf6909e874a1b16b3e3f23c53e7eec
TheGodzGaming <159502695+TheGodzGaming@users.noreply.github.com>
2024-02-08 21:39:22 -0500

    Add files via upload

---
+++ godz-newped_wtarget/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.displayText = false -- Whether you want the heading text to show or not
+Config.displayDistance = 20.0 -- From how far to start displaying text
+Config.displayColor = "~g~"
+
+Config.Peds = {
+    -- { x, y, z, ped heading, model hash, ped model, heading text, animation info }
+    {267.45, -624.04, 44.0, 65.94, 0x9D0087A8,"ig_claypain", "Gangster", "amb@world_human_aa_smoke@male@idle_a"}
+
+}
---
+++ godz-newped_wtarget/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'TheGodz'
+description 'NPC Spawning Script With QB-Target Sections Triggering Events'
+version '1.0.0'
+repository ''
+
+-- What to run
+client_scripts {
+    'config.lua',
+    'client/client.lua'
+}
+
+export 'addNPC'
