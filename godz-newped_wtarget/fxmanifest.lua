566491a6ff03b35b2b47d3764b4e50531cb60eba
TheGodzGaming <159502695+TheGodzGaming@users.noreply.github.com>
2024-02-08 22:36:42 -0500

    Update fxmanifest.lua

--- godz-newped_wtarget/fxmanifest.lua
+++ godz-newped_wtarget/fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'TheGodz'
-description 'NPC Spawning Script With QB-Target on Ped Model and Section that Triggering Events'
+description 'NPC Spawning Script With QB-Target on Ped Model, Section that Triggering Events'
 version '1.0.0'
 repository 'https://github.com/TheGodzGaming/5M-godz-newped-wtarget'
 
